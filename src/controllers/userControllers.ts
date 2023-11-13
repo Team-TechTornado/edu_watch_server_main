696bb20f18d3376899ae6baa2237fc58d877a5f5
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-13 12:58:00 +0900

    Feat. 유저의 모든 로그 조회하는 api 구현

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import UserLogs from "../models/UserLogs";
 
-//학부모의 전화번호를 받아 id 반환
+//GET 학부모의 전화번호를 받아 id 반환
 export const getUsers = async (req: Request, res: Response) => {
-  const phonenumber = req.query.phonenumber;
+  const { phonenumber } = req.params;
   const user = await User.findOne({ phoneNumber: phonenumber });
   if (!Boolean(user) || user === null) {
     return res.status(400).json(); //존재하지 않는 유저
@@ -18,6 +18,7 @@ export const getUsers = async (req: Request, res: Response) => {
   return res.status(200).json({ _id: user._id }); //유저 아이디 반환
 };
 
+// PUT _id를 가진 학생의 상태를 입실 상태로 User에서 갱신. 그리고 userLog에 추가.
 export const enterUser = async (req: Request, res: Response) => {
   const { _id } = req.body;
 
@@ -34,6 +35,7 @@ export const enterUser = async (req: Request, res: Response) => {
   return res.status(200).json();
 };
 
+// PUT _id를 가진 학생의 상태를 퇴실 상태로 User에서 갱신. 그리고 userLog에 추가.
 export const exitUser = async (req: Request, res: Response) => {
   const { _id } = req.body;
 
@@ -48,4 +50,15 @@ export const exitUser = async (req: Request, res: Response) => {
   userLogs!.logs.push({ entering: false });
   await userLogs!.save();
   return res.status(200).json();
+};
+
+// GET _id를 통해 모든 활동 기록 조회
+export const getUserLogAll = async (req: Request, res: Response) => {
+  const { _id } = req.params;
+
+  const userLogs = await UserLogs.findOne({ userId: _id });
+  if (!Boolean(userLogs) || userLogs == null) {
+    return res.status(400).json();
+  }
+  return res.status(200).json(userLogs.logs);
 };
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,15 @@
-import { getUsers, enterUser, exitUser } from "../controllers/userControllers";
+import {
+  getUsers,
+  enterUser,
+  exitUser,
+  getUserLogAll,
+} from "../controllers/userControllers";
 
 const express = require("express");
 const userRouter = express.Router();
 
-userRouter.get("/", getUsers);
+userRouter.get("/:phonenumber", getUsers);
 userRouter.put("/enter", enterUser);
 userRouter.put("/exit", exitUser);
+userRouter.get("/:_id/logs", getUserLogAll);
 export default userRouter;
